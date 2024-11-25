06192f3f334d384d843512797dc6b2f9af8319e5
leroy <leroynay@proton.me>
2024-11-25 12:40:41 -0700

    changed back to given code after tests

--- src/sleep.c
+++ src/sleep.c
@@ -5,23 +5,24 @@
 
 int main(void)
 {
-    stdio_init_all();
-    hard_assert(cyw43_arch_init() == PICO_OK);
-    while (1) {
-        sleep_ms(5000);
-        printf("Made it in sleep.c\n");
-    }
-    return 0;
-
-    // int toggle = 0;
+    // Test code to see if multiple executables are working
     // stdio_init_all();
-    // gpio_init(OUT_PIN);
-    // gpio_set_dir(OUT_PIN, GPIO_OUT);
-    // gpio_put(OUT_PIN, toggle);
-
-    // while (true) {
-    //     toggle = !toggle;
-    //     gpio_put(OUT_PIN, toggle);
-    //     sleep_ms(DELAY_MS);
+    // hard_assert(cyw43_arch_init() == PICO_OK);
+    // while (1) {
+    //     sleep_ms(5000);
+    //     printf("Made it in sleep.c\n");
     // }
+    // return 0;
+
+    int toggle = 0;
+    stdio_init_all();
+    gpio_init(OUT_PIN);
+    gpio_set_dir(OUT_PIN, GPIO_OUT);
+    gpio_put(OUT_PIN, toggle);
+
+    while (true) {
+        toggle = !toggle;
+        gpio_put(OUT_PIN, toggle);
+        sleep_ms(DELAY_MS);
+    }
 }
